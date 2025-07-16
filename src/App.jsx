f6d81888001b3183f6a2e48da62a04047a756246
jeel-bhoot <jeelbhoot0jb@gmail.com>
2025-07-16 22:36:30 +0530

    Add React Helment Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
+import data from "./Data/Data.json";
 
 export default function App() {
   return (
     <div className="App flex flex-col bg-light-white-third dark:bg-default font-sans tracking-tight">
       <BrowserRouter>
+        <Helmet>
+          <title>{data.app}</title>
+          <meta name="description" content={data.intro}></meta>
+        </Helmet>
         <Layout />
       </BrowserRouter>
     </div>
